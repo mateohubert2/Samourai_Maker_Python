1a0e480121c86fc5db0caee59555b7506217f282
mateohubert2 <mathub2003@gmail.com>
2023-05-14 10:42:52 +0200

    Mouvement du jouer droite gauche + saut

--- level.py
+++ level.py
@@ -4,7 +4,7 @@ from pygame.math import Vector2 as vector
 from parametres import*
 from support import*
 
-from sprites import Generic, Animated, Particule, Coin, Player, Ennemie2, Ennemie
+from sprites import Generic, Block, Animated, Particule, Coin, Player, Ennemie2, Ennemie
 
 class Level:
     def __init__(self, grid, switch, asset_dict):
@@ -15,7 +15,7 @@ class Level:
         self.all_sprites = pygame.sprite.Group()
         self.coin_sprites = pygame.sprite.Group()
         self.damage_sprites = pygame.sprite.Group()
-        
+        self.collision_sprites = pygame.sprite.Group()
         
         self.build_level(grid, asset_dict)
         
@@ -26,7 +26,7 @@ class Level:
           for layer_name, couche in grid.items():
               for pos, data in couche.items():
                     if layer_name == 'terrain':
-                        Generic(pos, asset_dict['land'][data], self.all_sprites)
+                        Generic(pos, asset_dict['land'][data], [self.all_sprites, self.collision_sprites])
                     if layer_name == 'eau':
                         if data == 'top':
                             Animated(asset_dict['water top'], pos, self.all_sprites)
@@ -34,17 +34,23 @@ class Level:
                             Generic(pos, asset_dict['water bottom'], self.all_sprites)
                     
                     match data:
-                        case 0: self.player = Player(pos, self.all_sprites)
+                        case 0: self.player = Player(pos, self.all_sprites, self.collision_sprites)
                         case 4: Coin('gold', asset_dict['gold'], pos, [self.all_sprites, self.coin_sprites])
                         case 5: Coin('silver', asset_dict['silver'], pos, [self.all_sprites, self.coin_sprites])
                         case 6: Coin('diamond', asset_dict['diamond'], pos, [self.all_sprites, self.coin_sprites])
                         
-                        case 7: Ennemie('left',asset_dict['ennemie'], pos, self.all_sprites)
+                        case 7: Ennemie('left',asset_dict['ennemie'], pos, [self.all_sprites, self.damage_sprites])
                         case 8: Ennemie2(asset_dict['ennemie2'], pos, [self.all_sprites, self.damage_sprites])
                         
-                        case 9: Animated(asset_dict['arbre']['Animation_Arbre1fg'], pos, self.all_sprites)
-                        case 10: Animated(asset_dict['arbre']['Animation_Arbre2fg'], pos, self.all_sprites)
-                        case 11: Animated(asset_dict['arbre']['Animation_Arbre3fg'], pos, self.all_sprites)
+                        case 9:
+                            Animated(asset_dict['arbre']['Animation_Arbre1fg'], pos, self.all_sprites)
+                            Block(pos + vector(25,11), (85, 45), self.collision_sprites)
+                        case 10:
+                            Animated(asset_dict['arbre']['Animation_Arbre2fg'], pos, self.all_sprites)
+                            Block(pos + vector(15,10), (95, 40), self.collision_sprites)
+                        case 11:
+                            Animated(asset_dict['arbre']['Animation_Arbre3fg'], pos, self.all_sprites)
+                            Block(pos + vector(15,25), (100, 60), self.collision_sprites)
                         
                         case 12: Animated(asset_dict['arbre']['Animation_Arbre1bg'], pos, self.all_sprites)
                         case 13: Animated(asset_dict['arbre']['Animation_Arbre2bg'], pos, self.all_sprites)
@@ -72,3 +78,4 @@ class Level:
         
         self.display_surface.fill(COULEUR_CIEL)
         self.all_sprites.draw(self.display_surface)
+        pygame.draw.rect(self.display_surface, 'yellow', self.player.hitbox)
--- sprites.py
+++ sprites.py
@@ -8,6 +8,11 @@ class Generic(pygame.sprite.Sprite):
         self.image = surf
         self.rect = self.image.get_rect(topleft = pos)
         
+class Block(Generic):
+    def __init__(self, pos, size, group):
+        surf = pygame.Surface(size)
+        super().__init__(pos, surf, group)
+        
 class Animated(Generic):
     def __init__(self, assets, pos, group):
         self.animation_frames = assets
@@ -64,14 +69,21 @@ class Ennemie(Generic):
         self.rect.bottom = self.rect.top + TAILLE_CASES
         
 class Player(Generic):
-    def __init__(self, pos, group):
-        super().__init__(pos, pygame.Surface((32,64)), group)
+    def __init__(self, pos, group, collision_sprites):
+        super().__init__(pos, pygame.Surface((80,64)), group)
         self.image.fill('red')
         
         #mouvement
         self.direction = vector()
-        self.pos = vector(self.rect.topleft)
+        self.pos = vector(self.rect.center)
         self.speed = 300
+        self.gravity = 4
+        self.on_floor = False
+        
+        #collision
+        self.collision_sprites = collision_sprites
+        self.hitbox = self.rect.inflate(-16,-14)
+        
         
     def input(self):
         keys =  pygame.key.get_pressed()
@@ -81,10 +93,48 @@ class Player(Generic):
             self.direction.x = -1
         else: self.direction.x = 0
         
+        if keys[pygame.K_SPACE] and self.on_floor:
+            self.direction.y = -2
+        
     def move(self, dt):
-        self.pos += self.direction * self.speed * dt
-        self.rect.topleft = (round(self.pos.x), round(self.pos.y))    
+        #horizontale
+        self.pos.x += self.direction.x * self.speed * dt
+        self.hitbox.centerx = round(self.pos.x)
+        self.rect.centerx = self.hitbox.centerx
+        self.collision('horizontal')
+        
+        #verticale
+        self.pos.y += self.direction.y * self.speed * dt
+        self.hitbox.centery = round(self.pos.y)
+        self.rect.centery = self.hitbox.centery
+        self.collision('vertical')
+    
+    def apply_gravity(self, dt):
+        self.direction.y += self.gravity * dt
+        self.rect.y += self.direction.y
+    
+    def check_on_floor(self):
+        floor_rect = pygame.Rect(self.hitbox.bottomleft,(self.hitbox.width,2))
+        floor_sprites = [sprite for sprite in self.collision_sprites if sprite.rect.colliderect(floor_rect)]
+        self.on_floor = True if floor_sprites else False
+    
+    def collision(self, direction):
+        for sprite in self.collision_sprites:
+            if sprite.rect.colliderect(self.hitbox):
+                if direction == 'horizontal':
+                    self.hitbox.right = sprite.rect.left if self.direction.x > 0 else self.hitbox.right
+                    self.hitbox.left = sprite.rect.right if self.direction.x < 0 else self.hitbox.left
+                    self.rect.centerx = self.hitbox.centerx
+                    self.pos.x = self.hitbox.centerx
+                else:
+                    self.hitbox.top = sprite.rect.bottom if self.direction.y < 0 else self.hitbox.top
+                    self.hitbox.bottom = sprite.rect.top if self.direction.y > 0 else self.hitbox.bottom
+                    self.rect.centery = self.hitbox.centery
+                    self.pos.y = self.hitbox.centery
+                    self.direction.y = 0
     
     def update(self, dt):
         self.input()
+        self.apply_gravity(dt)
         self.move(dt)
+        self.check_on_floor()
