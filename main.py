b3cda304d66c802a89135cd8e74a1a9ec662b377
evan <add.mail@serveur.com>
2023-05-15 15:16:46 +0200

    debut commentaires main

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ from level import Level
 from os import walk
 
 class Main:
+    #instantiation de la classe
     def __init__(self):
+        """_summary_
+        instancie les variables necessaires dans la classe main
+        """ 
         pygame.init()
         self.display_surface = pygame.display.set_mode((LARGEUR_FENETRE, HAUTEUR_FENETRE))
         self.clock = pygame.time.Clock()
@@ -17,35 +21,37 @@ class Main:
         self.editeur_active = True
         self.transition = Transition(self.toggle)
         self.editeur = Editeur(self.cases_terrain, self.switch)
-        
-        
-        #cureur
+        #importation et changement du cureur
+        #convert_alpha() permet d'augmenter les performances
         surf = load('Graphique/curseur/souris.png').convert_alpha()
         curseur = pygame.cursors.Cursor((0,0), surf)
         pygame.mouse.set_cursor(curseur)
-        
+   
     def imports(self):
+        """_summary_
+        importation des elements à aller chercher dans un dossier (image et sons)
+        on importera quelquefois un fichier complet pour ne pas avaoir à prendre toutes les images d'un fichier
+        on s'occupera de gerer ces fichiers plus tard
+        """
+        #terrain
         self.cases_terrain = import_folder_dict('Graphique/Terrain/Land')  
         self.bas_eau = load('Graphique/Eau/eau.png').convert_alpha()
         self.water_top_animation = import_folder('Graphique/Eau/Animations')
-        
         #piece
         self.gold = import_folder('Graphique/Piece/Animation_Piece_Gold')
         self.silver = import_folder('Graphique/Piece/Animation_Piece_Silver')
         self.diamond = import_folder('Graphique/Piece/Animation_Piece_Diamond')
         self.particule = import_folder('Graphique/Piece/Particule')
-        
-        #arbre
+        #arbre (fichier complet)
         self.arbre = {folder: import_folder(f'Graphique/Arbre/{folder}') for folder in list(walk('Graphique/Arbre'))[0][1]}
-        
+        #ennemis (fichier complet)
         self.ennemie2 = {folder: import_folder(f'Graphique/Ennemie/Animation_Ennemie_2/{folder}') for folder in list(walk('Graphique/Ennemie/Animation_Ennemie_2'))[0][1]}
         self.ennemie = {folder: import_folder(f'Graphique/Ennemie/Animation_Ennemie_1/{folder}') for folder in list(walk('Graphique/Ennemie/Animation_Ennemie_1'))[0][1]}
         self.pearl = load('Graphique/Ennemie/Pearl/pearl.png').convert_alpha()
-        #joueur
+        #joueur (fichier complet)
         self.player_graphics = {folder: import_folder(f'Graphique/Personnage/Animation_Personnage/{folder}') for folder in list(walk('Graphique/Personnage/Animation_Personnage'))[0][1]}
-        
+        #nuage
         self.clouds = import_folder('Graphique/Nuage')
-        
         #musique
         self.level_sounds = {
             'coin': pygame.mixer.Sound('audio/coin.wav'),
@@ -53,13 +59,21 @@ class Main:
             'jump': pygame.mixer.Sound('audio/jump.wav'),
             'music': pygame.mixer.Sound('audio/SuperHero.ogg'),
         }
-        
+
     def toggle(self):
+        """_summary_
+        boucle de musique de l'editeur sans fin tant qu'on est dans l'éditeur
+        """
         self.editeur_active = not self.editeur_active
         if self.editeur_active:
             self.editeur.editor_music.play(loops = -1)
-        
+   
     def switch(self, grid = None):
+        """_summary_
+        création d'un dictionnaire de tous les graphismes reutilisable par la suite
+        Args:
+            grid (_type_, optional): permet de recuperer les information d'une liste de donnée
+        """
         self.transition.active = True
         if grid:
             self.level = Level(grid, self.switch,{
@@ -79,7 +93,12 @@ class Main:
             self.level_sounds)
     
     def lancement(self):
+        """_summary_
+        definition des conditions de lancement de l'editeur ou du niveau
+        +lancement de l'editeur au début 
+        """
         while True:
+            #changer le nom de la fênetre de jeu
             pygame.display.set_caption('Samourai Maker par Matéo et Evan')
             dt = self.clock.tick() / 850
                     
@@ -92,6 +111,11 @@ class Main:
 
 class Transition:
     def __init__(self, toggle):
+        """_summary_
+        instancie les variables necessaires à la classe Transition
+        Args:
+            toggle (fonction definie plus haut): boucle de musique sans fin de l'editeur
+        """
         self.display_surface = pygame.display.get_surface()
         self.toggle = toggle
         self.active = False
@@ -101,20 +125,22 @@ class Transition:
         self.center = (LARGEUR_FENETRE / 2, HAUTEUR_FENETRE / 2)
         self.rayon = vector(self.center).magnitude()
         self.threshold = self.rayon + 100
-    
+    #condtions de la transition en 2 temps (faire grandir le cercle jusqu'au cenrtre puis repart en arrière)
     def display(self, dt):
         if self.active:
             self.largeur_bordure += 1000 * dt * self.direction
+            #augmente l'epaisseur du cercle jusqu'au centre
             if self.largeur_bordure >= self.threshold:
                 self.direction = -1
                 self.toggle()
+            #diminue l'epaisseur jusqu'au crecle initial
             if self.largeur_bordure < 0:
                 self.active = False
                 self.largeur_bordure = 0
                 self.direction = 1
             pygame.draw.circle(self.display_surface, 'black', self.center, self.rayon, int(self.largeur_bordure))
             
-
+#obligation de lancer le programme dans le main sinon ça ne va pas marcher
 if __name__ == '__main__':
     main = Main()
     main.lancement()
