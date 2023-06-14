55f3f4d7171c709c0299e093a94aa0c2df05edcc
evan <add.mail@serveur.com>
2023-06-14 15:10:48 +0200

    recuperation

--- level.py
+++ level.py
@@ -164,8 +164,6 @@ class Level:
             self.hit_sound.play()
             self.player.damage()
 
-
-        
     def boucle_evenement(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
